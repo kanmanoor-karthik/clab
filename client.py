663cbc4b97f1c5d96668d907386888ccfdbeaf9f
kanmanoor-karthik <kanmanoor.karthik@gmail.com>
2026-04-13 17:04:52 +0530

    Update client.py

--- client.py
+++ client.py
@@ -3,8 +3,6 @@ import threading
 import sys
 import readline
 import datetime
-hostname = socket.gethostname()
-ip_address = socket.gethostbyname(hostname)
 
 def receive(s):
     while True:
@@ -18,41 +16,35 @@ def receive(s):
             # get current typed input
             current = readline.get_line_buffer()
 
-            # move cursor to start and clear line
+            # clear current line
             sys.stdout.write("\r")
-            sys.stdout.write(" " * (len(current) + 4))
+            sys.stdout.write(" " * (len(current) + 10))
             sys.stdout.write("\r")
-            nowDate  = datetime.datetime.now()
-            # print incoming message
-            print()
-            print()
-            print()
-            print(nowDate,"      : "  ,msg, end="")
 
-            # redraw prompt + previous input
-            sys.stdout.write(">> " + current)
+            # print message cleanly
+            print(msg, end="")
+
+            # redraw input
+            sys.stdout.write("You: " + current)
             sys.stdout.flush()
 
         except:
             break
+
+
 def main():
-    hostx= input("Enter server address: ")
-    host = None
-    if hostx == "clab" : 
+    hostx = input("Enter server address: ")
+
+    if hostx == "clab":
         host = "10.4.3.142"
-        port=5000
-    else :
+        port = 5000
+    else:
         host = hostx
         port = int(input("Enter port: "))
-    if  not host and port:
-        print("Please fill up the host and port")
-        return
-    else :
-        pass
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
     s.connect((host, port))
-    
+
     # ---- USERNAME HANDSHAKE ----
     while True:
         prompt = s.recv(1024).decode()
@@ -68,8 +60,9 @@ def main():
         response = s.recv(1024).decode()
         if "OK" in response:
             break
+
         print(response)
-        print("username has been taken try restarting the file by clicking ctrl+c and reopen")
+        print("Username taken. Restart (Ctrl+C) and try again.")
 
     print("Connected. Start chatting...\n")
 
@@ -79,14 +72,15 @@ def main():
     # ---- SEND LOOP ----
     while True:
         try:
-            msg = input(">> ")
+            msg = input("You: ")
             if msg.lower() == "exit":
                 break
-            s.sendall(( msg + "\n").encode())
+            s.sendall((msg + "\n").encode())
         except KeyboardInterrupt:
             break
 
     s.close()
+
 
 if __name__ == "__main__":
     main()
